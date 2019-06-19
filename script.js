8cac9dced2a1ce858ffbe6d07de925049158e971
TahmidBinMahmud <1510927@iub.edu.bd>
2019-06-20 01:12:39 +0600

    Changes made after 12 am

--- script.js
+++ script.js
@@ -17,7 +17,20 @@ function move() {
         document.getElementsByClassName("button button4")[0].style.backgroundColor = "gray";
         document.getElementById("btn").innerText="Start Cooker";
       } else {
+        //document.getElementById('myBar').className = "w3-container w3-blue";
         width += 20;
+        if (width == 40){
+          document.getElementById('myBar').className = "w3-container w3-green";
+        }
+        if (width == 60){
+          document.getElementById('myBar').className = "w3-container w3-yellow";
+        }
+        if (width == 80){
+          document.getElementById('myBar').className = "w3-container w3-orange";
+        }
+        if (width == 100){
+          document.getElementById('myBar').className = "w3-container w3-red";
+        }
         elem.style.width = width + '%'; 
         document.getElementById("demo").innerHTML = width * 1  + '%';
       }
