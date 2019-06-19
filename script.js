20f6bc9cc4a1e744fa463403d3536dedd022c240
TahmidBinMahmud <1510927@iub.edu.bd>
2019-06-19 23:50:49 +0600

    Last Changes before 12am

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 /* Add your script here */
 
 function move() {
-    var elem = document.getElementById("myBar");   
-    var width = 20;
-    var id = setInterval(frame, 400);
+    var tempInitial = document.getElementById("num1").value;
+    var tempFinal = document.getElementById("num2").value;
+    document.getElementById("temp1").innerText= tempInitial + '° C';
+    document.getElementById("temp2").innerText= tempFinal + '° C';
+    var elem = document.getElementById("myBar");
+    var inter = document.getElementById("num3").value;
+    var width = 0;
+    var id = setInterval(frame, inter);
+    elem.style.width = width + '%'; 
+    document.getElementById("demo").innerHTML = width * 1  + '%';
     function frame() {
       if (width >= 100) {
         clearInterval(id);
+        document.getElementsByClassName("button button4")[0].style.backgroundColor = "gray";
+        document.getElementById("btn").innerText="Start Cooker";
       } else {
-        width += 20; 
+        width += 20;
         elem.style.width = width + '%'; 
         document.getElementById("demo").innerHTML = width * 1  + '%';
       }
     }
+  }
+
+  function submitButtonStyle(_this) {
+    _this.style.backgroundColor = "black";
+    document.getElementById("btn").innerText="Cooking";
   }
