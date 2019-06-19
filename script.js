22f096c934d6494ae7ffc0f5b42d45b65c99c941
TahmidBinMahmud <1510927@iub.edu.bd>
2019-06-19 18:42:32 +0600

    Work in progress

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /* Add your script here */
+
+function move() {
+    var elem = document.getElementById("myBar");   
+    var width = 20;
+    var id = setInterval(frame, 400);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width += 20; 
+        elem.style.width = width + '%'; 
+        document.getElementById("demo").innerHTML = width * 1  + '%';
+      }
+    }
+  }
